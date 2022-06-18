c118e49493a70e83787607b0c0a39ea7e5a9ab57
xiejon <jxie77@gmail.com>
2022-06-18 15:16:13 +0100

    tested and added hit() function and ship properties

--- src/scripts/__tests__/ship.test.js
+++ src/scripts/__tests__/ship.test.js
@@ -1,8 +1,24 @@
 import { Ship } from '../ship.js';
 
 test('ship contains a length', () => {
-    const expected = { length: 5 }
+    const expected = { length: 5 };
     const actual = Ship(5);
     expect(actual).toMatchObject(expected);
 });
 
+test('initializes hit locations corresponding to length', () => {
+    const expected = { hitLoc: {'0': false, '1': false} };
+    const actual = Ship(2);
+    expect(actual).toMatchObject(expected);
+});
+
+test('can access index in hitLoc', () => {
+    expect(Ship(2).hitLoc['1']).toBe(false);
+});
+
+test('hit() sets hitLoc index to true', () => {
+    const newShip = Ship(2);
+    newShip.hit(1);
+    const expected = { hitLoc: {'0': false, '1': true} };
+    expect(newShip).toMatchObject(expected);
+});
--- src/scripts/ship.js
+++ src/scripts/ship.js
@@ -1,8 +1,25 @@
 const Ship = (len) => {
     const length = len;
+    let hitLoc = initializeHitLocs(len);
+    let sunk = false;
+
     return {
-        length
+        length,
+        hitLoc,
+        sunk, 
+
+        hit(num) {
+            this.hitLoc[`${num}`] = true;
+        }
     }
+}
+
+function initializeHitLocs(length) {
+    let locs = [{}];
+    for (let i = 0; i < length; i++) {
+        locs[`${i}`] = false;
+    }
+    return locs;
 }
 
 module.exports = {
