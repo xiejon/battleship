c485fb72273e574a8c6b681a0372e962f7907408
xiejon <jxie77@gmail.com>
2022-06-19 14:55:00 +0100

    add & test function to place ship at given coordinates

--- src/scripts/__tests__/gameboard.test.js
+++ src/scripts/__tests__/gameboard.test.js
@@ -1,7 +1,15 @@
 import { Gameboard } from '../gameboard.js';
 
 test('has a 10x10 grid', () => {
-    expect(Gameboard().grid[0][0]).toBe(0);
-    expect(Gameboard().grid[5][8]).toBe(0);
-    expect(Gameboard().grid[9][9]).toBe(0);
+    expect(Gameboard().grid[0][0]).toBe(-1);
+    expect(Gameboard().grid[5][8]).toBe(-1);
+    expect(Gameboard().grid[9][9]).toBe(-1);
+});
+
+test('places ship at coordinates', () => {
+    const board = Gameboard();
+    board.placeShip(3, 0, 0);
+
+    expect(board.grid[0][0]).toBe(0)
+    expect(board.grid[2][0]).toBe(0)
 });
--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,21 +1,36 @@
 import { Ship } from "./ship.js";
 
 const Gameboard = () => {
+    let landscape = true;
     const grid = createGrid(10);
     
     return {
-        grid
+        landscape,
+        grid,
+
+        placeShip(len, xVal, yVal) {
+            const newShip = Ship(len);
+            
+            if (this.landscape === true) {
+                for (let i = 0; i < len; i++) {
+                    this.grid[i][yVal] = 0;
+                }
+            } else {
+                for (let i = 0; i < len; i++) {
+                    this.grid[xVal][i] = 0;
+                }
+            }
+        }
     }
 }
 
 function createGrid(num) {
     let grid = [];
-    // columns
-    for (let i = 0; i < num; i++) {
-        grid[i] = [];
-        // rows
-        for (let j = 0; j < num; j++) {
-            grid[i][j] = 0;
+    for (let col = 0; col < num; col++) {
+        grid[col] = [];
+
+        for (let row = 0; row < num; row++) {
+            grid[col][row] = -1;
         }
     }
     return grid;
