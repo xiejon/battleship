de1b4670db4c2016696f1d02687ee1876b02cad1
xiejon <jxie77@gmail.com>
2022-06-19 15:21:29 +0100

    wrote function to receive attack

--- src/scripts/__tests__/gameboard.test.js
+++ src/scripts/__tests__/gameboard.test.js
@@ -9,7 +9,23 @@ test('has a 10x10 grid', () => {
 test('places ship at coordinates', () => {
     const board = Gameboard();
     board.placeShip(3, 0, 0);
+    expect(board.grid[0][0]).toBe(0);
+    expect(board.grid[2][0]).toBe(0);
+    expect(board.grid[0][1]).toBe(-1);
+});
 
-    expect(board.grid[0][0]).toBe(0)
-    expect(board.grid[2][0]).toBe(0)
+test('ship correctly receives attack', () => {
+    const board = Gameboard();
+    board.placeShip(5, 0, 5);
+    board.receiveAttack(0, 5);
+    board.receiveAttack(4, 5);  
+    expect(board.grid[0][5]).toBe(1);
+    expect(board.grid[4][5]).toBe(1);
+});
+
+test('missed attack registers correctly', () => {
+    const board = Gameboard();
+    board.placeShip(5, 0, 5);
+    board.receiveAttack(0, 4);
+    expect(board.grid[0][4]).toBe(-1);
 });
--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,5 +1,9 @@
 import { Ship } from "./ship.js";
 
+// -1 = empty square
+// 0 = ship present
+// 1 = ship hit
+
 const Gameboard = () => {
     let landscape = true;
     const grid = createGrid(10);
@@ -8,17 +12,26 @@ const Gameboard = () => {
         landscape,
         grid,
 
-        placeShip(len, xVal, yVal) {
+        placeShip(len, x, y) {
             const newShip = Ship(len);
             
             if (this.landscape === true) {
                 for (let i = 0; i < len; i++) {
-                    this.grid[i][yVal] = 0;
+                    this.grid[i][y] = 0;
                 }
             } else {
                 for (let i = 0; i < len; i++) {
-                    this.grid[xVal][i] = 0;
+                    this.grid[x][i] = 0;
                 }
+            }
+            // return newShip?
+        },
+        receiveAttack(x, y) {
+            // const coords = this.grid[x][y];
+            if (this.grid[x][y] === -1) {
+                return;
+            } else if (this.grid[x][y] === 0) {
+                this.grid[x][y] = 1;
             }
         }
     }
