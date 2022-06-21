9aedf4b59a23cb45c482be4ead0062a195e48a09
xiejon <jxie77@gmail.com>
2022-06-21 18:02:59 +0100

    positioned grid

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -9,13 +9,34 @@ const Dom = () => {
         },
         createBoxes(grid, parent) {
             for (let col = 0; col < grid.length; col++) {
+                // Create columns to insert boxes into (for correct styling)
+                const column = document.createElement('div');
+
+                column.classList.add(`column`);
+                column.setAttribute('id', `${col}`)
+
+                parent.appendChild(column);
+
                 for (let row = 0; row < grid.length; row++) {
                     const box = document.createElement('div');
 
+                    box.classList.add('box');
                     box.setAttribute('id', `${col}`+ `${row}`);
 
-                    parent.appendChild(box);
+                    // temporary
+                    box.textContent = `${col}`+ `${row}`
+
+                    column.appendChild(box);
                 }
+            }
+        },
+        sortBoxes(boxes) {
+            for (let i = 0; i < 10; i++) {
+                const column = document.createElement('div');
+                column.classList.add(`column${i}`);
+            }
+            for (let i = 0; i < boxes.length; i++) {
+
             }
         }
     }    
