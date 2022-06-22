683e62ad0ba3747631df9bd232e56665e16fd089
xiejon <jxie77@gmail.com>
2022-06-22 22:57:34 +0100

    more styling

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -114,7 +114,7 @@ function showShip(e, user) {
                 if (x + i > 9) return;  // Prevents query selector from returning null
                 const box = document.querySelector(`[data-id="${x + i}${y}"]`)
                 if (box.hasAttribute('ship-present')) return;   // Do not highlight if there is a ship at the square
-                box.style.backgroundColor = (x + ship.length > 10) ? '#8B0000' : '#7A6440';
+                box.style.backgroundColor = (x + ship.length > 10) ? '#8B0000' : '#5C4033';
                 box.setAttribute('highlighted', '');
             }
         } else {
@@ -122,7 +122,7 @@ function showShip(e, user) {
                 if (y + i > 9) return;
                 const box = document.querySelector(`[data-id="${x}${y + i}"]`)
                 if (box.hasAttribute('ship-present')) return;
-                box.style.backgroundColor = (y + ship.length > 10) ? '#8B0000' : '#7A6440';
+                box.style.backgroundColor = (y + ship.length > 10) ? '#8B0000' : '#5C4033';
                 box.setAttribute('highlighted', '');
             }
         }
@@ -209,7 +209,7 @@ function createGrid(player, container) {
 function renderAttack(box, enemy, x, y) {
     if (enemy.board.grid[x][y] === -1) {
         // Missed shot
-        box.style.backgroundColor = '#7A6440';
+        box.style.backgroundColor = '#5C4033';
     } else {
         // Hit ship
         box.style.backgroundColor = '#8B0000';
@@ -233,6 +233,25 @@ function renderShips(player, gridContainer) {
         }
     }
 }
+
+function renderGithubIcon() {
+    const a = document.createElement('a');
+    a.href = 'https://github.com/xiejon';
+
+    const footer = document.querySelector('.footer')
+    const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const iconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
+    iconSvg.setAttribute('width', '24');
+    iconSvg.setAttribute('height', '24');
+    iconSvg.setAttribute('viewbox', '0 0 24 24');
+    iconPath.setAttribute('d', "M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z");
+
+    a.append(iconSvg);
+    iconSvg.appendChild(iconPath);
+    footer.append(a)
+}
+renderGithubIcon();
 
 
 export { renderBoards, getCoords, renderAttack, renderShips };
